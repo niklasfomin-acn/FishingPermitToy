8a3b09cf889bf403b356bafd97d23e9f26b69d3a
ConstantinJGruhl <constantin.gruhl@accenture.com>
2024-04-23 15:43:02 +0200

    Erste Version von GPT

--- client/utils/idDocument.go
+++ client/utils/idDocument.go
@@ -2,15 +2,16 @@ package utils
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"image"
 	"image/jpeg"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/textract"
 )
 
 type idDocument struct {
@@ -47,120 +48,70 @@ func (doc *idDocument) SelectDocument(FilePaths string) (File image.Image, err e
 	return File, nil
 }
 
-func (doc *idDocument) UploadDocument(File image.Image) (operationLocation string, err error) {
-	// Create a buffer to store the image
+func (doc *idDocument) UploadDocument(File image.Image) (JobId string, err error) {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1")}, // die Region anpassen
+	)
+
+	// Convert image.Image to []byte
 	buf := new(bytes.Buffer)
 	err = jpeg.Encode(buf, File, nil)
 	if err != nil {
-		return operationLocation, err
+		return JobId, err
 	}
 
-	// Write the POST request
-	req, err := http.NewRequest("POST", doc.Endpoint, buf)
+	// Create a Textract service client
+	svc := textract.New(sess)
+
+	// Call Textract
+	result, err := svc.AnalyzeDocument(&textract.AnalyzeDocumentInput{
+		Document: &textract.Document{
+			Bytes: buf.Bytes(),
+		},
+		FeatureTypes: []*string{
+			aws.String("FORMS"),
+		},
+	})
+
 	if err != nil {
-		return operationLocation, err
+		return JobId, err
 	}
 
-	// Set the headers
-	req.Header.Set("Content-Type", "application/octet-stream")
-	req.Header.Set("Ocp-Apim-Subscription-Key", doc.Key)
-
-	resp, err := doc.client.Do(req)
-	if err != nil {
-		return operationLocation, err
-	}
-	defer resp.Body.Close()
-
-	//log.Printf("Response: %v", resp)
-	operationLocation = resp.Header.Get("Operation-Location")
-	return operationLocation, nil
+	JobId = *result.JobId
+	return JobId, nil
 }
 
-func (doc *idDocument) GetResults(Endpoint string) (results string, err error) {
-	for {
-		req, err := http.NewRequest("GET", Endpoint, nil)
-		if err != nil {
-			return results, err
-		}
+func (doc *idDocument) GetResults(jobId string) (results *textract.GetDocumentAnalysisOutput, err error) {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1")}, // die Region anpassen
+	)
+	svc := textract.New(sess)
 
-		req.Header.Set("Ocp-Apim-Subscription-Key", doc.Key)
-		response, err := doc.client.Do(req)
-		if err != nil {
-			return results, err
-		}
-		defer response.Body.Close()
+	err = svc.GetDocumentAnalysisPages(&textract.GetDocumentAnalysisInput{
+		JobId: aws.String(jobId),
+	})
 
-		bodyBytes, err := io.ReadAll(response.Body)
-		if err != nil {
-			return results, err
-		}
-
-		var resp http.Response
-		err = json.Unmarshal(bodyBytes, &resp)
-		if err != nil {
-			return results, err
-		}
-
-		if resp.Status != "running" {
-			results = string(bodyBytes)
-			break
-		} else {
-			//log.Printf("Response not ready yet: %v", response)
-			time.Sleep(5 * time.Second)
-		}
-	}
-	return results, nil
+	return results, err
 }
 
-func (doc *idDocument) ParseResults(results string) (map[string]interface{}, error) {
-	var res map[string]interface{}
-	err := json.Unmarshal([]byte(results), &res)
-	if err != nil {
-		log.Printf("Error unmarshalling results: %v", err)
-		return res, nil
-	}
-
-	extractedData := make(map[string]interface{})
-
-	if analyzeResult, ok := res["analyzeResult"].(map[string]interface{}); ok {
-		if documents, ok := analyzeResult["documents"].([]interface{}); ok {
-			for _, document := range documents {
-				if docMap, ok := document.(map[string]interface{}); ok {
-					if fields, ok := docMap["fields"].(map[string]interface{}); ok {
-						extractedData = make(map[string]interface{})
-						for key, value := range fields {
-							if key == "FirstName" || key == "LastName" || key == "PlaceOfBirth" {
-								if valMap, ok := value.(map[string]interface{}); ok {
-									extractedData[key] = valMap["content"]
-								}
-							} else if key == "DateOfBirth" || key == "DateOfExpiration" {
-								if valMap, ok := value.(map[string]interface{}); ok {
-									extractedData[key] = valMap["valueDate"]
-								}
-							} else if key == "Address" {
-								if valMap, ok := value.(map[string]interface{}); ok {
-									extractedData[key] = valMap["valueAddress"]
-								}
-							} else if key == "DocumentNumber" || key == "EyeColor" || key == "Height" {
-								if valMap, ok := value.(map[string]interface{}); ok {
-									extractedData[key] = valMap["valueString"]
-								}
-
-							}
-
+func (doc *idDocument) ParseResults(results *textract.GetDocumentAnalysisOutput) (res map[string]interface{}, err error) {
+	for _, item := range results.Blocks {
+		if *item.BlockType == "KEY_VALUE_SET" {
+			if *item.EntityTypes[0] == "KEY" {
+				key := *item.Text
+				for _, valueID := range item.Value {
+					for _, valueItem := range results.Blocks {
+						if *valueItem.Id == *valueID {
+							res[key] = *valueItem.Text
+							break
 						}
-
-						//fmt.Println(extractedData)
 					}
 				}
 			}
 		}
-
 	}
-	//extractedMapToString := fmt.Sprintf("%v", extractedData)
-	//log.Printf("Extracted Data: %v", extractedMapToString)
-	//log.Printf("Extracted Data: %v", string(jsonStr))
-	return extractedData, nil
+
+	return res, nil
 }
 
 func (doc *idDocument) FormatResults(results map[string]interface{}) string {
